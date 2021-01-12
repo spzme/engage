7372c1aa59254268e0aadbe86bfd332336578dce
s1581538 <fgletharg@gmail.com>
2021-01-12 01:00:38 +0100

    Added performance testing, started making plots, interpreting etc.

--- EAXTester/Program.cs
+++ EAXTester/Program.cs
@@ -8,26 +8,6 @@ namespace EAXTester
 {
     class Program
     {
-        EAX.Parser parser;
         
-        static void Main(string[] args)
-        {
-            Program program = new Program();
-            program.TestParser();
-
-        }
-
-        public void TestParser()
-        {
-            string path = Path.Combine("..", "..", "..", "TestFile.xml");
-            string input = System.IO.File.ReadAllText(path);
-            //Console.WriteLine(input);
-            parser = new Parser("<input> <info> Contents </info> </input>");
-            var result = parser.Parse() as EAX.EngagedXmlDoc;
-            foreach (EAX.TagOpen tagOpen in result.tags)
-            {
-                Console.WriteLine(tagOpen);
-            }
-        }
     }
 }
